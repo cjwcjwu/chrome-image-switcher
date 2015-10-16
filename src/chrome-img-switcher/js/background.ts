6032fd24f4fe67522cad919fcfc157132b5cf5b5
snowcxt <snowcxt@gmail.com>
2015-10-16 09:50:51 -0700

    show popup window

--- src/chrome-img-switcher/js/background.ts
+++ src/chrome-img-switcher/js/background.ts
@@ -1,16 +1,10 @@
 // Add a click listener
 chrome.browserAction.onClicked.addListener((tab) => {
-    // Send a message to the content script
-    chrome.tabs.sendMessage(tab.id, { action: "toggle_editmode" }, function (response) {
-        if (response.edit_mode == 'on') {
-            // Update the tooltip and add badge text
-            chrome.browserAction.setTitle({ 'tabId': tab.id, 'title': 'Edit Mode: ON' });
-            chrome.browserAction.setBadgeText({ 'tabId': tab.id, 'text': 'On' });
-        } else {
-            // Update the tooltip and remove the badge text
-            chrome.browserAction.setTitle({ 'tabId': tab.id, 'title': 'Edit Mode: Off' });
-            chrome.browserAction.setBadgeText({ 'tabId': tab.id, 'text': '' });
-        }
+    chrome.windows.create({
+        url: "popup/index.html",
+        type: "popup",
+        width: 1150,
+        height: 700
     });
     
 });
